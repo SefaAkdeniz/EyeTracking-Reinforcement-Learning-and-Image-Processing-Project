1e0cb10bc8ee8107c2802d45cd724362ea13fb37
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2020-09-05 16:05:14 +0300

    finished scan_face script

---
+++ scan_face.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 15:50:45 2020
+
+@author: sefa
+"""
+
+import cv2
+import numpy as np
+
+haarcascade = "haarcascade_frontalface_alt2.xml"
+haarcascade_clf = "data/" + haarcascade
+detector = cv2.CascadeClassifier(haarcascade_clf)
+
+LBFmodel = "LFBmodel.yaml"
+LBFmodel_file = "data/" + LBFmodel
+landmark_detector  = cv2.face.createFacemarkLBF()
+landmark_detector.loadModel(LBFmodel_file)
+
+webcam_cap = cv2.VideoCapture(0)
+while(True):
+    _, frame = webcam_cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector.detectMultiScale(gray)
+    
+    for (x,y,w,d) in faces:
+        
+        _, landmarks = landmark_detector.fit(gray, np.array(faces))
+        
+        for landmark in landmarks:
+           
+            for x,y in landmark[0]:
+    
+                cv2.circle(frame, (x, y), 1, (255, 0, 0), 2)
+
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(20) & 0xFF  == ord('q'):
+        webcam_cap.release()
+        cv2.destroyAllWindows()
+        break
