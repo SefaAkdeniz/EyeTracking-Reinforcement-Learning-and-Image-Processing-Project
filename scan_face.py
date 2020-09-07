2e9f456e66db8ade3f4049614debb9943e74ae1a
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2020-09-07 15:40:30 +0300

    eye points selected

--- scan_face.py
+++ scan_face.py
@@ -8,6 +8,11 @@ Created on Sat Sep  5 15:50:45 2020
 import cv2
 import numpy as np
 
+eye_point=[36,37,38,39,40,41,42,43,44,45,46,47]
+
+left_eye =[]
+right_eye =[]
+
 haarcascade = "haarcascade_frontalface_alt2.xml"
 haarcascade_clf = "data/" + haarcascade
 detector = cv2.CascadeClassifier(haarcascade_clf)
@@ -30,23 +35,25 @@ while(True):
         _, landmarks = landmark_detector.fit(gray, np.array(faces))
         
         for landmark in landmarks:
-           
+            count=0
+            left_eye.clear()
+            right_eye.clear()
             for x,y in landmark[0]:
-    
-                cv2.circle(frame, (x, y), 1, (255, 0, 0), 2)
-
+                
+                if(count in eye_point):
+                
+                    if(count<42):
+                        
+                        left_eye.append((x,y))
+                    else:
+                        
+                        right_eye.append((x,y))
+                        
+                    cv2.circle(frame, (x, y), 1, (255, 0, 0), 2)
+                count+=1
     cv2.imshow("Camera", frame)
 
     if cv2.waitKey(20) & 0xFF  == ord('q'):
         webcam_cap.release()
         cv2.destroyAllWindows()
         break
-    
-
-
-import turtle
-
-n = 60
-pen=turtle.Turtle()
-
-pen.circle(10)
